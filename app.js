d6aeffbc1a8fe268ddedde306aed0d199b76e261
chen <chensandiego@gmail.com>
2017-06-07 11:54:34 +0800

    change jade layout

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ mongoose.connect('mongodb://localhost/sitesearch');
 var db=mongoose.connection;
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var websites = require('./routes/websites');
+var articles = require('./routes/articles');
 
 var app = express();
 
@@ -28,8 +29,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/websites', websites);
 
+app.use('/articles', articles);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
